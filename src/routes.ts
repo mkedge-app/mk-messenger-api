eeb05e9eb11271ce055b219ff0fd02dd5b5cc5bb
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-11 22:23:16 -0400

    Utilização de middleware nas rotas protegidas

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,19 @@
 import { Router } from 'express';
-
+import { AuthenticateTenant } from './middlewares/auth';
 import TenantController from './app/controllers/TenantController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = Router();
 
-// Rotas dos tenants
-routes.get('/tenants', TenantController.index);
-routes.post('/tenants', TenantController.create);
-routes.get('/tenants/:id', TenantController.show);
-routes.delete('/tenants/:id', TenantController.delete);
-
 // Rota de autenticação
 routes.post('/session', SessionController.create);
+
+// Rotas dos tenants
+routes.get('/tenants', AuthenticateTenant, TenantController.index);
+routes.post('/tenants', AuthenticateTenant, TenantController.create);
+routes.get('/tenants/:id', AuthenticateTenant, TenantController.show);
+routes.delete('/tenants/:id', AuthenticateTenant, TenantController.delete);
+
+
 
 export default routes;
