1b10e32e22c394967b42eff5aa922550f3f79bb0
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-11 21:42:34 -0400

    Definição de rotas para expor métodos do tenantcontroller

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
-import HelloWorldController from './app/controllers/HelloWorldController';
+import TenantController from './app/controllers/TenantController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = Router();
 
-routes.get('/', HelloWorldController.index);
+// Rotas dos tenants
+routes.get('/tenants', TenantController.index);
+routes.post('/tenants', TenantController.create);
+routes.get('/tenants/:id', TenantController.show);
+routes.delete('/tenants/:id', TenantController.delete);
+
+// Rota de autenticação
+routes.post('/session', SessionController.create);
 
 export default routes;
