da206ae29af409bdfa33b6dd1719629a9557300f
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-11 21:42:45 -0400

    Deleção de arquivos de exemplo

--- src/app/controllers/HelloWorldController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-class HelloWorldController {
-  async index(req: Request, res: Response) {
-    return res.json({ ok: true });
-  }
-}
-
-export default new HelloWorldController();
