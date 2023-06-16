88f705f39ce390b1c77ff194794a07d4d934e86c
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-16 14:38:35 -0400

    Aplicação de middlewares utilizando Router do express

--- src/routes.ts
+++ src/routes.ts
@@ -11,24 +11,20 @@ const routes = Router();
 // Rota de autenticação
 routes.post("/session", SessionController.create);
 
+// Aplicar middleware de autenticação
+routes.use(authenticateTenant);
+
 // Rotas dos tenants
-routes.get("/tenants", authenticateTenant, TenantController.index);
+routes.get("/tenants", TenantController.index);
 routes.post("/tenants", TenantController.create);
-routes.get("/tenants/:id", authenticateTenant, TenantController.show);
-routes.delete("/tenants/:id", authenticateTenant, TenantController.delete);
+routes.get("/tenants/:id", TenantController.show);
+routes.delete("/tenants/:id", TenantController.delete);
+
+// Aplicar middleware de autenticação
+routes.use(tenantStatusCheck);
 
 // Rotas de interação com o WhatsApp
-routes.post(
-  "/whatsapp/sessions",
-  authenticateTenant,
-  tenantStatusCheck,
-  WhatsAppController.create
-);
-routes.get(
-  "/whatsapp/sessions/:key",
-  authenticateTenant,
-  tenantStatusCheck,
-  WhatsAppController.show
-);
+routes.post("/whatsapp/sessions", WhatsAppController.create);
+routes.get("/whatsapp/sessions/:key", WhatsAppController.show);
 
 export default routes;
