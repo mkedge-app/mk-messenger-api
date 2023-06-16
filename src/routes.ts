e6bd498ff3c79c96212a80e5896b4c5f9b40b12e
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-16 13:40:44 -0400

    Alteração do nome do middleware de autenticação do tenant

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+
 import SessionController from "./app/controllers/SessionController";
 import TenantController from "./app/controllers/TenantController";
 import WhatsAppController from "./app/controllers/WhatsAppController";
-import { AuthenticateTenant } from "./middlewares/auth";
+
+import { authenticateTenant } from "./middlewares/auth";
 import { tenantStatusCheck } from "./middlewares/tenantStatusCheck";
 
 const routes = Router();
@@ -11,21 +13,21 @@ const routes = Router();
 routes.post("/session", SessionController.create);
 
 // Rotas dos tenants
-routes.get("/tenants", AuthenticateTenant, TenantController.index);
+routes.get("/tenants", authenticateTenant, TenantController.index);
 routes.post("/tenants", TenantController.create);
-routes.get("/tenants/:id", AuthenticateTenant, TenantController.show);
-routes.delete("/tenants/:id", AuthenticateTenant, TenantController.delete);
+routes.get("/tenants/:id", authenticateTenant, TenantController.show);
+routes.delete("/tenants/:id", authenticateTenant, TenantController.delete);
 
 // Rotas de interação com o WhatsApp
 routes.post(
   "/whatsapp/sessions",
-  AuthenticateTenant,
+  authenticateTenant,
   tenantStatusCheck,
   WhatsAppController.create
 );
 routes.get(
   "/whatsapp/sessions/instances",
-  AuthenticateTenant,
+  authenticateTenant,
   tenantStatusCheck,
   WhatsAppController.index
 );
