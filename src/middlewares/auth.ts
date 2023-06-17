64a93ad185468f94b3c3f7e71f50c9795710a6d5
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-17 11:59:36 -0400

    Refatoração do middleware de autenticação

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { VerifyErrors } from "jsonwebtoken";
 import { JWT_CONFIG } from "../config/jwt";
 
 interface AuthenticatedRequest extends Request {
@@ -12,29 +12,28 @@ interface DecodedToken {
   isTenantActive: boolean;
 }
 
-// Middleware para autenticação do tenantId
-export const authenticateTenant = (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-): Response<any, Record<string, any>> | void => {
-  const authHeader = req.headers.authorization;
+type AuthenticateTenantResponse = Response<Record<string, any>> | void;
 
+export const authenticateTenant = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<AuthenticateTenantResponse> => {
+  // Verificar se o cabeçalho de autorização (Authorization) está presente na requisição
+  const authHeader = req.headers.authorization;
   if (!authHeader) {
     return res.status(401).json({ error: "Token não fornecido" });
   }
 
+  // Verifica se o cabeçalho de autenticação contém um token válido
   const [scheme, token] = authHeader.split(" ");
-
   if (!scheme || !token || scheme.toLowerCase() !== "bearer") {
     return res.status(401).json({ error: "Token inválido" });
   }
 
   try {
+    // Verifica e decodifica o token
     const decodedToken = jwt.verify(token, JWT_CONFIG.secret) as DecodedToken;
 
-    if (!decodedToken.tenantId) {
-      return res.status(401).json({ error: "Token inválido" });
+    // Verifica se existe um valor para tenantId e isTenantActive dentro do token
+    if (!decodedToken.hasOwnProperty('tenantId') || !decodedToken.hasOwnProperty('isTenantActive')) {
+      return res.status(401).json({ error: "O token não contém as informações necessárias" });
     }
 
     // Adiciona o tenantId ao objeto de requisição para uso posterior
@@ -44,6 +43,19 @@ export const authenticateTenant = (
     // Continua para o próximo middleware ou rota
     next();
   } catch (error) {
-    return res.status(401).json({ error: "Token inválido" });
+    const jwtError = error as VerifyErrors;
+
+    if (jwtError.name === "JsonWebTokenError") {
+      return res.status(401).json({ error: "Token inválido" });
+    } else if (jwtError.name === "NotBeforeError") {
+      return res.status(401).json({ error: "Token ainda não é válido" });
+    } else if (jwtError.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token expirado" });
+    } else if (jwtError.name === "SignatureVerificationError") {
+      return res.status(401).json({ error: "Erro na verificação da assinatura do token" });
+    }
+
+    // Tratamento de outros erros não específicos
+    return res.status(500).json({ error: "Erro ao processar o token" });
   }
 };
