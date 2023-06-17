9e16289f149667165a14c66c1bc0e45f1d7a66f6
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-17 13:03:23 -0400

    Implementação de registro de logs

--- src/app/controllers/SessionController.ts
+++ src/app/controllers/SessionController.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import Tenant from '../models/Tenant';
 import { JWT_CONFIG } from '../../config/jwt';
+import logger from '../../logger';
 
 class SessionController {
   async create(req: Request, res: Response) {
@@ -12,12 +13,14 @@ class SessionController {
       // Verificar se o usuário existe
       const tenant = await Tenant.findOne({ usuario });
       if (!tenant) {
+        logger.info(`Usuário não encontrado: ${usuario}`);
         return res.status(401).json({ error: 'Usuário não encontrado' });
       }
 
       // Verificar se a senha está correta
       const senhaCorreta = await bcrypt.compare(senha, tenant.senha);
       if (!senhaCorreta) {
+        logger.info(`Senha incorreta para o usuário: ${usuario}`);
         return res.status(401).json({ error: 'Senha incorreta' });
       }
 
@@ -26,9 +29,12 @@ class SessionController {
 
       const token = jwt.sign(payload, JWT_CONFIG.secret, { expiresIn: JWT_CONFIG.expiresIn });
 
+      logger.info(`Autenticação bem-sucedida para o usuário: ${usuario}`);
+
       // Autenticação bem-sucedida
       return res.status(200).json({ token, expiresIn: parseInt(JWT_CONFIG.expiresIn) });
     } catch (error) {
+      logger.error(`Erro ao autenticar o tenant: ${error}`);
       return res.status(500).json({ error: 'Erro ao autenticar o tenant' });
     }
   }
