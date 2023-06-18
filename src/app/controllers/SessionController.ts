63c1cca74456f3f16aa8b2b05786d01b4492f2df
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-18 00:03:22 -0400

    Remoção de registros de log

--- src/app/controllers/SessionController.ts
+++ src/app/controllers/SessionController.ts
@@ -13,14 +13,12 @@ class SessionController {
       // Verificar se o usuário existe
       const tenant = await Tenant.findOne({ usuario });
       if (!tenant) {
-        logger.info(`Usuário não encontrado: ${usuario}`);
         return res.status(401).json({ error: 'Usuário não encontrado' });
       }
 
       // Verificar se a senha está correta
       const senhaCorreta = await bcrypt.compare(senha, tenant.senha);
       if (!senhaCorreta) {
-        logger.info(`Senha incorreta para o usuário: ${usuario}`);
         return res.status(401).json({ error: 'Senha incorreta' });
       }
 
@@ -29,12 +27,10 @@ class SessionController {
 
       const token = jwt.sign(payload, JWT_CONFIG.secret, { expiresIn: JWT_CONFIG.expiresIn });
 
-      logger.info(`Autenticação bem-sucedida para o usuário: ${usuario}`);
 
       // Autenticação bem-sucedida
       return res.status(200).json({ token, expiresIn: parseInt(JWT_CONFIG.expiresIn) });
     } catch (error) {
-      logger.error(`Erro ao autenticar o tenant: ${error}`);
       return res.status(500).json({ error: 'Erro ao autenticar o tenant' });
     }
   }
