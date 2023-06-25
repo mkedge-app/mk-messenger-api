8e7bcbb03ccc082655007324d51741536d6b5c49
Edilson Lima <edilsonlm217@hotmail.com>
2023-06-24 20:10:55 -0400

    Implementação de documentação e refatoração de código

--- src/services/WhatsAppSessionManager.ts
+++ src/services/WhatsAppSessionManager.ts
@@ -5,6 +5,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import { Subject } from 'rxjs';
 import { QRCodeData } from '../types/WhatsAppApi';
+import FileUtils from './FileUtils';
 
 interface SocketMap {
   [name: string]: any;
@@ -13,12 +14,48 @@ interface SocketMap {
 class WhatsAppSessionManager {
   private socks: SocketMap = {};
   private qrCodeSubject: Subject<QRCodeData>;
+  private fileUtils: FileUtils;
 
   constructor() {
     this.qrCodeSubject = new Subject<QRCodeData>();
+    this.fileUtils = new FileUtils();
     logger.info('WhatsAppSessionManager inicializado');
   }
 
+  /**
+   * Restaura as sessões existentes a partir dos arquivos armazenados no diretório tokens.
+   * Cria uma sessão para cada diretório não vazio encontrado.
+   *
+   * @returns Uma Promise que é resolvida quando todas as sessões foram inicializadas.
+   */
+  public async initSessions(): Promise<void> {
+    logger.info('Restaurando sessões existentes...');
+    const tokensFolder = path.resolve(__dirname, '..', '..', 'tokens');
+    const folderNames = await fs.readdir(tokensFolder);
+
+    for (const folderName of folderNames) {
+      const sessionName = folderName;
+      const sessionFolderPath = path.join(tokensFolder, sessionName);
+      const sessionFolderContent = await fs.readdir(sessionFolderPath);
+
+      if (sessionFolderContent.length > 0) {
+        logger.info(`Iniciando sessão de ${sessionName}...`);
+        await this.createSession(sessionName);
+      } else {
+        logger.info(`O diretório da sessão ${sessionName} está vazio. A sessão não será iniciada.`);
+      }
+    }
+    logger.info('Restaurando sessões finalizou');
+  }
+
+  /**
+   * Cria uma nova sessão do WhatsApp com base no nome fornecido.
+   * A sessão é criada usando o estado de autenticação obtido do arquivo correspondente aos tokens da sessão.
+   *
+   * @param name O nome da sessão a ser criada.
+   * @throws Um erro é lançado se o nome da sessão não for fornecido.
+   * @returns Uma Promise que é resolvida quando a sessão é criada com sucesso.
+   */
   public async createSession(name: string): Promise<void> {
     if (!name) {
       throw new Error("O nome da sessão é obrigatório");
@@ -29,6 +66,17 @@ class WhatsAppSessionManager {
     this.createSocketWhatsApp(name, state, saveCreds);
   }
 
+  public getQrCodeObservable(): Subject<QRCodeData> {
+    return this.qrCodeSubject;
+  }
+
+  /**
+   * Cria um socket do WhatsApp e configura os eventos necessários.
+   *
+   * @param name - O nome da sessão.
+   * @param authState - O estado de autenticação do WhatsApp.
+   * @param saveCreds - Uma função para salvar as credenciais.
+   */
   private createSocketWhatsApp(name: string, authState: any, saveCreds: () => void): void {
     logger.info(`Criando sessão para ${name}...`);
     const socketWhatsApp = makeWASocket({ printQRInTerminal: true, auth: authState });
@@ -48,6 +96,12 @@ class WhatsAppSessionManager {
     this.socks[name] = socketWhatsApp;
   }
 
+  /**
+   * Manipula as atualizações de conexão do socket do WhatsApp.
+   *
+   * @param name - O nome da sessão associada ao socket.
+   * @param update - As informações da atualização de conexão.
+   */
   private handleConnectionUpdate(name: string, update: any): void {
     logger.info(`Atualização de conexão do socket de ${name} recebida`);
 
@@ -90,9 +144,14 @@ class WhatsAppSessionManager {
     }
   }
 
+  /**
+   * Manipula o evento de logout de uma sessão do WhatsApp.
+   *
+   * @param name - O nome da sessão que foi desconectada.
+   */
   private handleLoggedOut(name: string): void {
     const tokensFolderPath = this.resolveTokensFolderPath(name);
-    this.deleteFolderRecursive(tokensFolderPath);
+    this.fileUtils.deleteFolderRecursive(tokensFolderPath);
 
     delete this.socks[name];
   }
@@ -100,44 +159,6 @@ class WhatsAppSessionManager {
   private resolveTokensFolderPath(name: string): string {
     const tokensFolderPath = path.resolve(__dirname, '..', '..', 'tokens', name);
     return tokensFolderPath;
-  }
-
-  private deleteFolderRecursive(folderPath: string): void {
-    fs.remove(folderPath)
-      .then(() => {
-        logger.info(`Pasta excluída com sucesso: ${folderPath}`);
-      })
-      .catch((error) => {
-        logger.error(`Erro ao excluir pasta: ${error}`);
-      });
-  }
-
-  public getActiveSocks(): SocketMap {
-    return this.socks;
-  }
-
-  public getQrCodeObservable(): Subject<QRCodeData> {
-    return this.qrCodeSubject;
-  }
-
-  public async initSessions(): Promise<void> {
-    logger.info('Restaurando sessões existentes...');
-    const tokensFolder = path.resolve(__dirname, '..', '..', 'tokens');
-    const folderNames = await fs.readdir(tokensFolder);
-
-    for (const folderName of folderNames) {
-      const sessionName = folderName;
-      const sessionFolderPath = path.join(tokensFolder, sessionName);
-      const sessionFolderContent = await fs.readdir(sessionFolderPath);
-
-      if (sessionFolderContent.length > 0) {
-        logger.info(`Iniciando sessão de ${sessionName}...`);
-        await this.createSession(sessionName);
-      } else {
-        logger.info(`O diretório da sessão ${sessionName} está vazio. A sessão não será iniciada.`);
-      }
-    }
-    logger.info('Restaurando sessões finalizou');
   }
 }
 
