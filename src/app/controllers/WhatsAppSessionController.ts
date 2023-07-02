ba108ce334ce012811983440d037b5ba32fca71b
Edilson Lima <edilsonlm217@hotmail.com>
2023-07-01 20:14:54 -0400

    Definição do método show para obter uma sessão por nome

--- src/app/controllers/WhatsAppSessionController.ts
+++ src/app/controllers/WhatsAppSessionController.ts
@@ -6,6 +6,17 @@ class WhatsAppSessionController {
     const sessions = WhatsAppSessionManager.getSessions();
     return res.json(sessions);
   }
+
+  async show(req: Request, res: Response) {
+    const name = req.params.name;
+    const session = WhatsAppSessionManager.getSessionByName(name);
+
+    if (session) {
+      return res.json(session);
+    } else {
+      return res.status(404).json({ error: 'Sessão não encontrada' });
+    }
+  }
 }
 
 export default new WhatsAppSessionController();
