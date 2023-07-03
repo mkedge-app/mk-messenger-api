846ad6b4c83fa25b1d1afd281c06dbf0dec5f7bc
Edilson Lima <edilsonlm217@hotmail.com>
2023-07-03 12:26:02 -0400

    Melhoria na semantica do código

--- src/modules/whatsapp/WhatsAppSocket.ts
+++ src/modules/whatsapp/WhatsAppSocket.ts
@@ -3,12 +3,12 @@ import logger from '../../logger';
 import { Boom } from '@hapi/boom';
 import { Subject } from 'rxjs';
 
-type WASocket = ReturnType<typeof makeWASocket> | undefined;
+type WASocket = ReturnType<typeof makeWASocket>;
 type VoidResponse = void;
 type QrCodeSubjectResponse = { qrcode: string };
 
 class WhatsAppSocket {
-  private socket: WASocket;
+  private socket!: WASocket;
   private connectionUpdateSubject = new Subject<Partial<ConnectionState>>();
   private qrCodeSubject = new Subject<QrCodeSubjectResponse>();
   private connectionOpenedSubject = new Subject<VoidResponse>();
@@ -16,30 +16,30 @@ class WhatsAppSocket {
   private connectionRestartRequiredSubject = new Subject<VoidResponse>();
   private connectionBadSessionSubject = new Subject<VoidResponse>();
   private connectionClosedSubject = new Subject<VoidResponse>();
-  private connectionLostSubject = new Subject<VoidResponse>();
   private connectionReplacedSubject = new Subject<VoidResponse>();
   private multideviceMismatchSubject = new Subject<VoidResponse>();
   private timedOutSubject = new Subject<VoidResponse>();
 
-  constructor(private readonly name: string) { }
+  constructor(private readonly name: string) {
+
+  }
 
   public async create(): Promise<void> {
     logger.info(`[WhatsAppSocket] Criando WASocket para ${this.name}...`);
+    await this.initializeSocket();
+    this.listenForConnectionUpdates();
+  }
 
-    // Obtém o estado de autenticação e a função de salvar credenciais
+  private async initializeSocket(): Promise<void> {
     const { state, saveCreds } = await useMultiFileAuthState(`tokens/${this.name}`);
-
-    // Cria o socket WASocket com as opções fornecidas
-    this.socket = makeWASocket({ printQRInTerminal: true, auth: state, qrTimeout: 20000 });
-
-    // Listener para atualizações de credenciais
+    this.socket = makeWASocket({ printQRInTerminal: true, auth: state });
     this.socket.ev.on('creds.update', () => {
       saveCreds();
     });
+  }
 
+  private listenForConnectionUpdates(): void {
     logger.info(`[WhatsAppSocket] Ouvindo atualizações de conexão para o WASocket de ${this.name}`);
-
-    // Listener para atualizações de conexão
     this.socket.ev.on('connection.update', (update: Partial<ConnectionState>) => {
       this.handleConnectionUpdate(update);
     });
@@ -47,63 +47,49 @@ class WhatsAppSocket {
 
   private handleConnectionUpdate(update: Partial<ConnectionState>): void {
     logger.info(`[WhatsAppSocket] Atualização de conexão do socket de ${this.name} recebida...`);
-
     const { connection, lastDisconnect } = update;
     const statusCode = (lastDisconnect?.error as Boom)?.output?.statusCode;
 
     if (connection === 'close') {
       logger.info(`[WhatsAppSocket] A conexão com o socket de ${this.name} foi fechada`);
       this.socket = undefined;
-      this.handleConnectionClosed(statusCode);
+      if (statusCode) {
+        this.handleConnectionClosed(statusCode);
+      }
     } else if (connection === 'open') {
       logger.info(`[WhatsAppSocket] A conexão com o socket de ${this.name} está aberta`);
-      this.connectionOpenedSubject.next();
+      this.connectionUpdateSubject.next(update);
     } else if ('qr' in update && update.qr) {
-      // Emite o QR Code para os assinantes interessados
-      this.qrCodeSubject.next({ qrcode: update.qr });
+      this.emitQrCode(update.qr);
     }
 
     this.connectionUpdateSubject.next(update);
   }
 
-  private handleConnectionClosed(statusCode?: number): void {
-    switch (statusCode) {
-      case DisconnectReason.loggedOut:
-        logger.info(`[WhatsAppSocket] Motivo: Logout`);
-        this.connectionLoggedOutSubject.next();
-        break;
-      case DisconnectReason.restartRequired:
-        logger.info(`[WhatsAppSocket] Motivo: Reinicialização necessária`);
-        this.connectionRestartRequiredSubject.next();
-        break;
-      case DisconnectReason.badSession:
-        logger.info(`[WhatsAppSocket] Motivo: badSession`);
-        this.connectionBadSessionSubject.next();
-        break;
-      case DisconnectReason.connectionClosed:
-        logger.info(`[WhatsAppSocket] Motivo: Conexão fechada`);
-        this.connectionClosedSubject.next();
-        break;
-      case DisconnectReason.connectionLost:
-        logger.info(`[WhatsAppSocket] Motivo: Conexão perdida`);
-        this.connectionLostSubject.next();
-        break;
-      case DisconnectReason.connectionReplaced:
-        logger.info(`[WhatsAppSocket] Motivo: connectionReplaced`);
-        this.connectionReplacedSubject.next();
-        break;
-      case DisconnectReason.multideviceMismatch:
-        logger.info(`[WhatsAppSocket] Motivo: multideviceMismatch`);
-        this.multideviceMismatchSubject.next();
-        break;
-      case DisconnectReason.timedOut:
-        logger.info(`[WhatsAppSocket] Motivo: timedOut`);
-        this.timedOutSubject.next();
-        break;
-      default:
-        break;
+  private handleConnectionClosed(statusCode: number): void {
+    const subjects: { [key: number]: Subject<void> } = {
+      [DisconnectReason.loggedOut]: this.connectionLoggedOutSubject,
+      [DisconnectReason.restartRequired]: this.connectionRestartRequiredSubject,
+      [DisconnectReason.badSession]: this.connectionBadSessionSubject,
+      [DisconnectReason.connectionClosed]: this.connectionClosedSubject,
+      // [DisconnectReason.connectionLost]: this.connectionLostSubject,
+      [DisconnectReason.connectionReplaced]: this.connectionReplacedSubject,
+      [DisconnectReason.multideviceMismatch]: this.multideviceMismatchSubject,
+      [DisconnectReason.timedOut]: this.timedOutSubject,
+    };
+
+    const subject = subjects[statusCode];
+    if (subject) {
+      logger.info(`[WhatsAppSocket] Motivo: ${DisconnectReason[statusCode]}`);
+      subject.next();
     }
   }
+
+  private emitQrCode(qrCode: string): void {
+    this.qrCodeSubject.next({ qrcode: qrCode });
+  }
+
+  // Métodos para obter os subjects
 
   public getConnectionUpdateSubject(): Subject<Partial<ConnectionState>> {
     return this.connectionUpdateSubject;
@@ -131,10 +117,6 @@ class WhatsAppSocket {
 
   public getConnectionClosedSubject(): Subject<VoidResponse> {
     return this.connectionClosedSubject;
-  }
-
-  public getConnectionLostSubject(): Subject<VoidResponse> {
-    return this.connectionLostSubject;
   }
 
   public getConnectionReplacedSubject(): Subject<VoidResponse> {
