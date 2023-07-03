f6d14201f58b8ef56def685a4c866a89918a0bd2
Edilson Lima <edilsonlm217@hotmail.com>
2023-07-03 11:51:10 -0400

    Implementação inicial com subjects definidos

--- src/modules/whatsapp/WhatsAppSocket.ts
+++ src/modules/whatsapp/WhatsAppSocket.ts
@@ -4,13 +4,39 @@ import { Boom } from '@hapi/boom';
 import { Subject } from 'rxjs';
 
 type WASocket = ReturnType<typeof makeWASocket> | undefined;
+type VoidResponse = void;
+
+interface QrCodeSubjectResponse {
+  qrcode: string
+}
 
 class WhatsAppSocket {
   private socket: WASocket;
   private connectionUpdateSubject: Subject<Partial<ConnectionState>>;
+  private qrCodeSubject: Subject<QrCodeSubjectResponse>;
+  private connectionOpenedSubject: Subject<VoidResponse>;
+  private connectionLoggedOutSubject: Subject<VoidResponse>;
+  private connectionRestartRequiredSubject: Subject<VoidResponse>;
+  private connectionBadSessionSubject: Subject<VoidResponse>;
+  private connectionClosedSubject: Subject<VoidResponse>;
+  private connectionLostSubject: Subject<VoidResponse>;
+  private connectionReplacedSubject: Subject<VoidResponse>;
+  private multideviceMismatchSubject: Subject<VoidResponse>;
+  private timedOutSubject: Subject<VoidResponse>;
+
 
   constructor(private readonly name: string) {
     this.connectionUpdateSubject = new Subject<Partial<ConnectionState>>();
+    this.qrCodeSubject = new Subject<QrCodeSubjectResponse>();
+    this.connectionOpenedSubject = new Subject<VoidResponse>();
+    this.connectionLoggedOutSubject = new Subject<VoidResponse>();
+    this.connectionRestartRequiredSubject = new Subject<VoidResponse>();
+    this.connectionBadSessionSubject = new Subject<VoidResponse>();
+    this.connectionClosedSubject = new Subject<VoidResponse>();
+    this.connectionLostSubject = new Subject<VoidResponse>();
+    this.connectionReplacedSubject = new Subject<VoidResponse>();
+    this.multideviceMismatchSubject = new Subject<VoidResponse>();
+    this.timedOutSubject = new Subject<VoidResponse>();
   }
 
   public async create(): Promise<void> {
@@ -41,7 +67,10 @@ class WhatsAppSocket {
       this.handleConnectionClosed(statusCode);
     } else if (connection === 'open') {
       logger.info(`[WhatsAppSocket] A conexão com o socket de ${this.name} está aberta`);
-      // Lógica para lidar com a conexão estabelecida
+      this.connectionOpenedSubject.next();
+    } else if ('qr' in update && update.qr) {
+      // Emite o QR Code para os assinantes interessados
+      this.qrCodeSubject.next({ qrcode: update.qr });
     }
 
     this.connectionUpdateSubject.next(update);
@@ -51,35 +80,35 @@ class WhatsAppSocket {
     switch (statusCode) {
       case DisconnectReason.loggedOut:
         logger.info(`[WhatsAppSocket] Motivo: Logout`);
-        // Lógica para lidar com o logout
+        this.connectionLoggedOutSubject.next();
         break;
       case DisconnectReason.restartRequired:
         logger.info(`[WhatsAppSocket] Motivo: Reinicialização necessária`);
-        // Lógica para lidar com a reinicialização necessária
+        this.connectionRestartRequiredSubject.next();
         break;
       case DisconnectReason.badSession:
         logger.info(`[WhatsAppSocket] Motivo: badSession`);
-        // Lógica para lidar com uma sessão inválida
+        this.connectionBadSessionSubject.next();
         break;
       case DisconnectReason.connectionClosed:
         logger.info(`[WhatsAppSocket] Motivo: Conexão fechada`);
-        // Lógica para lidar com uma conexão fechada
+        this.connectionClosedSubject.next();
         break;
       case DisconnectReason.connectionLost:
         logger.info(`[WhatsAppSocket] Motivo: Conexão perdida`);
-        // Lógica para lidar com uma conexão perdida
+        this.connectionLostSubject.next();
         break;
       case DisconnectReason.connectionReplaced:
         logger.info(`[WhatsAppSocket] Motivo: connectionReplaced`);
-        // Lógica para lidar com uma conexão substituída
+        this.connectionReplacedSubject.next();
         break;
       case DisconnectReason.multideviceMismatch:
         logger.info(`[WhatsAppSocket] Motivo: multideviceMismatch`);
-        // Lógica para lidar com um conflito de dispositivos múltiplos
+        this.multideviceMismatchSubject.next();
         break;
       case DisconnectReason.timedOut:
         logger.info(`[WhatsAppSocket] Motivo: timedOut`);
-        // Lógica para lidar com o tempo limite
+        this.timedOutSubject.next();
         break;
       default:
         break;
