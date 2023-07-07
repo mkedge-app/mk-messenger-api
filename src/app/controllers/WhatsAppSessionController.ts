53e14f2b8ccf4126a33c5244c3c47e511267b55b
Edilson Lima <edilsonlm217@hotmail.com>
2023-07-07 19:12:25 -0400

    Implementação de endpoint para deleção de sessões

--- src/app/controllers/WhatsAppSessionController.ts
+++ src/app/controllers/WhatsAppSessionController.ts
@@ -17,6 +17,20 @@ class WhatsAppSessionController {
       return res.status(404).json({ error: 'Sessão não encontrada' });
     }
   }
+
+  async delete(req: Request, res: Response) {
+    const name = req.params.name;
+    const session = WhatsAppSessionManager.getSessionByName(name);
+
+    if (session) {
+      // Chame o método para excluir a sessão aqui
+      WhatsAppSessionManager.deleteSession(name);
+
+      return res.json({ message: 'Sessão excluída com sucesso' });
+    } else {
+      return res.status(404).json({ error: 'Sessão não encontrada' });
+    }
+  }
 }
 
 export default new WhatsAppSessionController();
