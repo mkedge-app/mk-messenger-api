b308d54ec7aac64b193259a8aaa6e1bc77c5f4f5
Edilson Lima <edilsonlm217@hotmail.com>
2023-07-24 18:19:50 -0400

    Implementação de documentação

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Arquivo de inicialização da aplicação
+ *
+ * Este arquivo é responsável por iniciar a aplicação, estabelecer a conexão com o banco de dados
+ * e iniciar o servidor HTTP e WebSocket.
+ */
+
 import dotenv from 'dotenv';
 import AppServer from './app';
 import Database from './database';
@@ -5,24 +12,36 @@ import logger from './logger';
 
 dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
+/**
+ * Função de inicialização da aplicação
+ *
+ * Esta função é responsável por iniciar a aplicação, estabelecer a conexão com o banco de dados
+ * e iniciar o servidor HTTP e WebSocket.
+ */
 async function startApp() {
+  // Obter a porta da variável de ambiente
   const port = process.env.PORT;
 
+  // Verificar se a variável de ambiente PORT está definida
   if (!port) {
     logger.error('A variável de ambiente PORT não está definida. A inicialização da aplicação será interrompida.');
     process.exit(1); // Encerra a aplicação com código de erro (1)
   }
 
   try {
+    // Conectar ao banco de dados
     const database = new Database();
     await database.connect();
 
+    // Inicializar o servidor HTTP e WebSocket
     const appServer = new AppServer();
     appServer.start(Number(port));
   } catch (error: any) {
+    // Tratar erros durante a inicialização
     logger.error(error.message);
     process.exit(1); // Encerra a aplicação com código de erro (1)
   }
 }
 
+// Iniciar a aplicação chamando a função startApp()
 startApp();
