9851ab59d4d38df9dadbf1f0e19de9868eb84e9a
Edilson Lima <edilsonlm217@hotmail.com>
2023-07-24 18:18:24 -0400

    Melhoria na definição da inicialização do app

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import dotenv from 'dotenv';
 import AppServer from './app';
 import Database from './database';
+import logger from './logger';
 
 dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 async function startApp() {
+  const port = process.env.PORT;
+
+  if (!port) {
+    logger.error('A variável de ambiente PORT não está definida. A inicialização da aplicação será interrompida.');
+    process.exit(1); // Encerra a aplicação com código de erro (1)
+  }
+
   try {
     const database = new Database();
     await database.connect();
 
     const appServer = new AppServer();
-    const port = 3000;
-    appServer.start(port);
+    appServer.start(Number(port));
   } catch (error: any) {
-    console.error(error.message);
+    logger.error(error.message);
     process.exit(1); // Encerra a aplicação com código de erro (1)
   }
 }
