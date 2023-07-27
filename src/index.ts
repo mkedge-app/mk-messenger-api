3b367e6a06d5fce905666f3e8be80a70372bd197
Antonio Brito <edilsonlm217@hotmail.com>
2023-07-27 19:38:28 +0000

    Refatoração

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
 import dotenv from 'dotenv';
-import AppServer from './app';
-import logger from './logger';
+import App from './app';
 
 dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 async function startApp() {
-  // Obter a porta da variável de ambiente
-  const port = process.env.PORT;
-
-  // Verificar se a variável de ambiente PORT está definida
-  if (!port) {
-    logger.error('A variável de ambiente PORT não está definida. A inicialização da aplicação será interrompida.');
-    process.exit(1); // Encerra a aplicação com código de erro (1)
-  }
-
-  try {
-    // Inicializar o servidor HTTP e WebSocket
-    const appServer = new AppServer();
-    await appServer.start(Number(port));
-  } catch (error: any) {
-    // Tratar erros durante a inicialização
-    logger.error(error.message);
-    process.exit(1); // Encerra a aplicação com código de erro (1)
-  }
+  const appServer = new App();
+  await appServer.start();
 }
 
 // Iniciar a aplicação chamando a função startApp()
