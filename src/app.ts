7b84ac5fa5815522abc23d55ac90376528446727
Antonio Brito <edilsonlm217@hotmail.com>
2023-07-27 20:13:53 +0000

    Refatoração do códio para deixa-lo mais semantivo

--- src/app.ts
+++ src/app.ts
@@ -13,118 +13,83 @@ import Database from "./database";
 class App {
   private app: express.Application;
   private httpServer!: http.Server;
-  private httpsServer!: https.Server;
-  private httpWebSocketServer: WebSocket.Server;
-  private httpsWebSocketServer: WebSocket.Server | undefined;
+  private https!: https.Server;
+  private httpWebSocketServer!: WebSocket.Server; // Declare without initializer
+  private httpsWebSocketServer?: WebSocket.Server; // Declare without initializer
   private database: Database;
 
   constructor() {
     this.app = express();
     this.database = new Database();
-    this.setupHttpServer();
-    this.setupHttpsServer();
-    this.httpWebSocketServer = new WebSocket.Server({ server: this.httpServer });
-    new WebSocketServer(this.httpWebSocketServer);
     this.setupMiddlewares();
     this.setupRoutes();
+    this.setupHttpServer();
+    this.setupHttpsServer();
   }
 
-  /**
-   * Método privado para configurar o servidor HTTPS em ambiente de produção.
-   * Verifica se as variáveis de ambiente HTTPS_PRIVATE_KEY_PATH e HTTPS_CERTIFICATE_PATH
-   * estão definidas e cria o servidor HTTPS com as chaves privadas e certificados fornecidos.
-   * Caso ocorra algum erro na configuração, registra o erro no log e encerra a aplicação.
-   * Este método é modificado para be executed only in production.
-   */
-  private setupHttpsServer(): void {
-    if (process.env.NODE_ENV !== "production") {
-      logger.info("[AppServer]: Skipping HTTPS server setup in non-production environment.");
-      return;
-    }
-
-    const privateKeyPath = process.env.HTTPS_PRIVATE_KEY_PATH;
-    const certificatePath = process.env.HTTPS_CERTIFICATE_PATH;
-
-    if (!privateKeyPath || !certificatePath) {
-      console.error("The environment variables HTTPS_PRIVATE_KEY_PATH and HTTPS_CERTIFICATE_PATH must be defined in production environment.");
-      process.exit(1);
-    }
-
-    const httpsOptions = {
-      key: fs.readFileSync(privateKeyPath),
-      cert: fs.readFileSync(certificatePath),
-    };
-
-    try {
-      // Create the https server only in production
-      this.httpsServer = https.createServer(httpsOptions, this.app);
-      logger.info(`[AppServer]: HTTPS Server configured on port ${process.env.HTTPS_PORT}`);
-
-      // Create the WebSocket server for httpsServer
-      this.httpsWebSocketServer = new WebSocket.Server({ server: this.httpsServer });
-      new WebSocketServer(this.httpsWebSocketServer);
-    } catch (error: any) {
-      console.error(`[AppServer]: Error configuring HTTPS server: ${error.message}`);
-      process.exit(1);
-    }
-  }
-
-  /**
-   * Método privado para configurar o servidor HTTP em ambiente de desenvolvimento.
-   * Cria o servidor HTTP padrão.
-   */
   private setupHttpServer(): void {
     this.httpServer = http.createServer(this.app);
+    this.httpWebSocketServer = new WebSocket.Server({ server: this.httpServer }); // Create WebSocket server for HTTP
+    new WebSocketServer(this.httpWebSocketServer);
     logger.info(`[AppServer]: HTTP Server configured on port ${process.env.HTTP_PORT}`);
   }
 
-  /**
-   * Método privado para habilitar os middlewares da aplicação.
-   * Habilita o middleware 'cors' para permitir solicitações de origens diferentes
-   * e o middleware para analisar corpos de requisição JSON.
-   */
+  private setupHttpsServer(): void {
+    if (process.env.NODE_ENV === "production") {
+      const privateKeyPath = process.env.HTTPS_PRIVATE_KEY_PATH;
+      const certificatePath = process.env.HTTPS_CERTIFICATE_PATH;
+
+      if (!privateKeyPath || !certificatePath) {
+        console.error("The environment variables HTTPS_PRIVATE_KEY_PATH and HTTPS_CERTIFICATE_PATH must be defined in the production environment.");
+        process.exit(1);
+      }
+
+      const httpsOptions = {
+        key: fs.readFileSync(privateKeyPath),
+        cert: fs.readFileSync(certificatePath),
+      };
+
+      try {
+        this.https = https.createServer(httpsOptions, this.app);
+        this.httpsWebSocketServer = new WebSocket.Server({ server: this.https }); // Create WebSocket server for HTTPS
+        new WebSocketServer(this.httpsWebSocketServer);
+        logger.info(`[AppServer]: HTTPS Server configured on port ${process.env.HTTPS_PORT}`);
+      } catch (error: any) {
+        console.error(`[AppServer]: Error configuring HTTPS server: ${error.message}`);
+        process.exit(1);
+      }
+    } else {
+      logger.info("[AppServer]: Skipping HTTPS server setup in non-production environment.");
+    }
+  }
+
   private setupMiddlewares(): void {
     this.app.use(cors());
     this.app.use(express.json());
   }
 
-  /**
-   * Método privado para configurar as rotas da aplicação.
-   * Utiliza as rotas definidas no arquivo routes.ts
-   */
   private setupRoutes(): void {
     this.app.use(routes);
   }
 
-  /**
-   * Método público para iniciar o servidor.
-   * Conecta ao banco de dados e inicia o servidor HTTP/HTTPS na porta especificada.
-   * Aguarda a inicialização do servidor e restaura as sessões do WhatsApp.
-   * Caso ocorra algum erro na inicialização, registra o erro no log e encerra a aplicação.
-   * @param port O número da porta em que o servidor será iniciado.
-   * Este método is modificado to be executed only in production.
-   */
   public async start(): Promise<void> {
     try {
-      await this.database.connect(); // Connect to the database
-
-      // Start HTTP server on port process.env.HTTP_PORT
+      await this.database.connect();
       this.httpServer.listen(process.env.HTTP_PORT, async () => {
         logger.info(`[AppServer]: HTTP Server started on port ${process.env.HTTP_PORT}`);
         logger.info("[AppServer]: WebSocket Server for HTTP started");
         await WhatsAppSessionManager.restoreSessions();
       });
 
-      // Start HTTPS server only in production
-      if (process.env.NODE_ENV === "production") {
-        this.httpsServer.listen(process.env.HTTPS_PORT, async () => {
+      if (process.env.NODE_ENV === "production" && this.httpsWebSocketServer) {
+        this.https.listen(process.env.HTTPS_PORT, async () => {
           logger.info(`[AppServer]: HTTPS Server started on port ${process.env.HTTPS_PORT}`);
           logger.info("[AppServer]: WebSocket Server for HTTPS started");
         });
       }
     } catch (error: any) {
       logger.error(`[AppServer]: Failed to start the servers: ${error.message}`);
-      process.exit(1); // Exit the application with an error code (1)
+      process.exit(1);
     }
   }
 }
