7d12b16ba264091995bd894242a9e64262c2e03e
Edilson Lima <edilsonlm217@hotmail.com>
2023-08-03 03:46:02 -0400

    Definição do método createDefaultAdminUser

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import logger from '../logger';
+import bcrypt from 'bcrypt';
+import AdminUserModel from '../app/models/AdminUser';
 
 import MONGO_DB_URL from '../config/mongodb';
 
 class Database {
   constructor() {}
 
-  async connect(): Promise<void> {
+  async connectAndCreateDefaultAdminUser(): Promise<void> {
     logger.info('[Database]: Connecting to MongoDB...');
 
     try {
@@ -16,9 +18,36 @@ class Database {
       });
 
       logger.info('[Database]: Connected to MongoDB successfully');
+      await this.createDefaultAdminUser();
     } catch (error: any) {
       logger.error(`[Database]: Connection Error: ${error.message}`);
       throw new Error('Failed to connect to the database. Check the database connection settings.');
+    }
+  }
+
+  private async createDefaultAdminUser(): Promise<void> {
+    try {
+      const adminUser = await AdminUserModel.findOne({ username: process.env.DEFAULT_ADMIN_USERNAME });
+
+      if (!adminUser) {
+        const plainPassword = process.env.DEFAULT_ADMIN_PASSWORD;
+        if (plainPassword) { // Verifique se plainPassword não é undefined
+          const hashedPassword = await bcrypt.hash(plainPassword, 10);
+
+          await AdminUserModel.create({
+            username: process.env.DEFAULT_ADMIN_USERNAME,
+            password_hash: hashedPassword,
+            email: 'admin@example.com',
+            role: 'admin'
+          });
+
+          logger.info('Default admin user created successfully.');
+        } else {
+          logger.error('DEFAULT_ADMIN_PASSWORD is undefined.');
+        }
+      }
+    } catch (error: any) {
+      logger.error(`Error creating default admin user: ${error.message}`);
     }
   }
 }
