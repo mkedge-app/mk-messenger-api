b3c28c7f5f631325c7ed52d8fb3b49d73651c82c
Edilson Lima <edilsonlm217@hotmail.com>
2023-08-16 12:36:46 -0400

    Implementacao de paginacao no metodo index

--- src/app/controllers/MessageLogController.ts
+++ src/app/controllers/MessageLogController.ts
@@ -1,25 +1,34 @@
 import { Request, Response } from "express";
 import logger from "../../logger";
-import MessageLogService from "../../services/MessageLogService";
+import MessageLogService, { PaginationResult } from "../../services/MessageLogService";
+import { IMessage } from "../models/Message";
 
 class MessageLogController {
   async index(req: Request, res: Response) {
     try {
-      // Obter todas as mensagens do log do banco de dados usando o MessageLogService
-      const messages = await MessageLogService.getAllMessages();
+      const { page = 1, limit = 10 } = req.query;
 
-      // Verificar se há mensagens encontradas
-      if (messages.length === 0) {
+      const paginationResult: PaginationResult<IMessage> = await MessageLogService.getAllMessagesWithPagination(
+        +page,
+        +limit
+      );
+
+      if (paginationResult.data.length === 0) {
         return res.status(404).json({ message: "Nenhuma mensagem encontrada no log" });
       }
 
-      // Retornar as mensagens como resposta em formato JSON
-      return res.json(messages);
+      return res.json({
+        currentPage: paginationResult.currentPage,
+        totalPages: paginationResult.totalPages,
+        totalMessages: paginationResult.totalMessages,
+        messages: paginationResult.data,
+      });
     } catch (error) {
       logger.info("[MessageLogController]: Erro ao listar as mensagens do log:", error);
       return res.status(500).json({ error: "Erro ao listar as mensagens do log" });
     }
   }
+
 
   async show(req: Request, res: Response) {
     try {
