5c01ce956089ef0812bc82dee3741b2771b3b1fd
Edilson Lima <edilsonlm217@hotmail.com>
2023-08-19 14:02:49 -0400

    Implementacao de comentarios e validacao de parametros

--- src/app/controllers/SessionController.ts
+++ src/app/controllers/SessionController.ts
@@ -8,30 +8,40 @@ class SessionController {
   async create(req: Request, res: Response) {
     const { username, password } = req.body;
 
+    // Verifica se os parâmetros "username" e "password" são fornecidos no corpo da solicitação.
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Os parâmetros "username" e "password" são obrigatórios no corpo da solicitação' });
+    }
+
     try {
-      // Verificar se o usuário existe
+      // Verificar se o usuário existe no banco de dados.
       const user = await User.findOne({ username });
+
+      // Se o usuário não for encontrado, retorna um erro de autenticação.
       if (!user) {
         return res.status(401).json({ error: 'Usuário não encontrado' });
       }
 
-      // Verificar se a senha está correta
+      // Verificar se a senha fornecida corresponde à senha armazenada no banco de dados.
       const senhaCorreta = await bcrypt.compare(password, user.passwordHash);
+
+      // Se a senha estiver incorreta, retorna um erro de autenticação.
       if (!senhaCorreta) {
         return res.status(401).json({ error: 'Senha incorreta' });
       }
 
-      // Gerar o token JWT
+      // Gerar um token JWT com base nas informações do usuário.
       const payload = { userId: user.id, userType: user.userType };
-
       const token = jwt.sign(payload, JWT_CONFIG.secret, { expiresIn: JWT_CONFIG.expiresIn });
 
-      // Autenticação bem-sucedida, retorne também o userType
+      // Responde com sucesso e inclui o token JWT, o ID do usuário e o tipo de usuário.
       return res.status(200).json({ userId: user.id, userType: user.userType, token, expiresIn: parseInt(JWT_CONFIG.expiresIn) });
     } catch (error) {
+      // Em caso de erro interno, retorna um erro interno do servidor.
       return res.status(500).json({ error: 'Erro ao autenticar o usuário' });
     }
   }
+
 }
 
 export default new SessionController();
