de30f38f6948977d12dcaca57a817d1523045d0c
Edilson Lima <edilsonlm217@hotmail.com>
2023-08-19 15:43:15 -0400

    Definiçao da interface Invoice

--- src/modules/mercado-pago/types/index.ts
+++ src/modules/mercado-pago/types/index.ts
@@ -2,3 +2,26 @@
 export interface EventMapping {
   [eventType: string]: (eventData: any) => Promise<void>;
 }
+
+export interface Invoice {
+  _id: string;
+  __v: number;
+  currency_id: string;
+  date_created: string;
+  debit_date: string;
+  id: number;
+  last_modified: string;
+  next_retry_date: string;
+  payment: {
+    id: number;
+    status: string;
+    status_detail: string;
+  };
+  payment_method_id: string;
+  preapproval_id: string;
+  reason: string;
+  retry_attempt: number;
+  status: string;
+  transaction_amount: number;
+  type: string;
+}
