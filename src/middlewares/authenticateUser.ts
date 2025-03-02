15a6e0cddaa0c4698e4de0d7850b2a900ef40b59
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-02 12:35:58 -0400

    Melhorado a lógica de autenticação

--- src/middlewares/authenticateUser.ts
+++ src/middlewares/authenticateUser.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response } from 'express';
 import { VerifyErrors } from 'jsonwebtoken';
 import AuthUtils from '../services/AuthUtils';
 import { AuthenticatedRequest } from '../types/authentication';
+import User from '../app/models/User'; // Importa o modelo de usuário
 
 export const authenticateUser = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
@@ -24,8 +25,15 @@ export const authenticateUser = async (req: AuthenticatedRequest, res: Response,
       return AuthUtils.handleMissingInfo(res);
     }
 
+    // Verifica se o usuário ainda existe
+    const userExists = await User.findById(userId);
+    if (!userExists) {
+      return res.status(401).json({ error: 'Usuário não encontrado ou removido.' });
+    }
+
     req.userId = userId;
     req.userType = userType;
+    req.userStatus = userExists.status;
 
     next();
   } catch (error) {
