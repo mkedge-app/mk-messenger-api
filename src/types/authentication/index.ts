90b11f9d54be1ea0e7fdac9ffda314e49e3642e5
Edilson R Lima <edilson.lima@meta.com.br>
2025-03-02 12:38:31 -0400

    Adicionado userStatus a interface que representa a request

--- src/types/authentication/index.ts
+++ src/types/authentication/index.ts
@@ -3,6 +3,7 @@ import { Request } from "express";
 export interface AuthenticatedRequest extends Request {
   userId?: string;
   userType?: string;
+  userStatus?: string;
 }
 
 export interface DecodedToken {
